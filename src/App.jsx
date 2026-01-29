2db142e06c7065c90ecece4aa012dd17b6ee3f3a
IrfanPatelIG <irfanpatel1997abc@gmail.com>
2026-01-29 17:15:49 +0530

    Changed the Theme

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,10 @@ function App() {
 
   const TodoCard = ({Data}) => {
     return <>
-      <li className='task-lis *:h-full'>
-        <div className='task-num flex-text-center w-6 pl-2'>{Data.id}</div>
-        <div className='task-todo flex-text-center p-1'>{Data.task}</div>
-        <div className="actions flex items-center  gap-2 h-full p-1 *:w-[36px] *:h-[36px] *:transition-[background-image] *:duration-400 *:ease-out">
+      <li className='task-lis'>
+        <div className='task-num flex-text-center w-6 h-full pl-2'>{Data.id}</div>
+        <div className='task-todo flex-text-center w-full h-full p-1'>{Data.task}</div>
+        <div className="actions flex items-center gap-3 h-full p-1 *:w-[30px] *:h-[30px] *:transition-[background-image] *:duration-400 *:ease-out">
           <span className='edit-task cursor-pointer hover:bg-[url("./assets/square-pen-fill.svg")] hover:animate-rotate360 bg-[url("./assets/square-pen-nofill.svg")] bg-cover bg-no-repeat bg-center'></span>
           <span ref={checkBtn} onClick={() => ToggleTask(Data.id)} className='check-task cursor-pointer bg-cover bg-no-repeat bg-center hover:animate-rotate360 bg-[url("./assets/check-circle-nofill.svg")] hover:bg-[url("./assets/check-circle-fill.svg")]'>
             
@@ -49,22 +49,22 @@ function App() {
 
   return (
     <>
-      <div className='main-container w-screen h-screen font-arimo'>
+      <div className='main-container w-screen h-screen font-arimo overflow-x-hidden box-border'>
         <NavBar />
 
         <div className="w-[98%] h-[90vh] mx-[1vw] my-[1vh] rounded-lg bg-linear-to-br/decreasing from-indigo-500 to-teal-40">
-          <main className="hero-section w-fit p-1.5 m-auto translate-y-[25%] flex flex-col gap-3.5">
+          <main className="hero-section w-fit p-1.5 m-auto translate-y-[25%] flex flex-col items-center gap-3.5 overflow-hidden *:overflow-hidden">
             <div className="task w-fit h-12 m-auto overflow-hidden flex rounded-[28px]">
-              <input ref={inputRef} type="text" name="task" placeholder='What to do?' className='w-lg px-4 py-2 text-white text-[18px] bg-[#1018286f] focus:outline-0' />
-              <button className='prime-btn w-32 text-xl'>Add</button>
+              <input ref={inputRef} type="text" name="task" placeholder='What to do?' className='w-lg px-4 py-2 text-white text-[19px] bg-[#1018286f] focus:outline-0' />
+              <button className='w-32 prime-btn-og'>Add</button>
             </div>
 
-            <div className="tasks-todo flex flex-col gap-3">
+            <div className="tasks-todo w-[94%] flex flex-col gap-3">
               <div className="switch-tab flex items-center justify-center gap-2.5 *:rounded-md *:border-2 *:border-bg-prime-2 *:px-2 *:py-0.5 *:hover:bg-bg-prime-1-30 *:cursor-pointer">
                 <span>pending tasks</span>
                 <span>completed tasks</span>
               </div>
-              <ul role='list' className='tasks-list w-[98%] max-h-[340px] pb-1 flex flex-col items-start gap-2 m-auto overflow-y-auto'>
+              <ul role='list' className='tasks-list w-full max-h-[340px] pb-1 flex flex-col items-start gap-2 overflow-y-auto'>
                 {
                   taskData.map((taskD) => {
                     return <TodoCard key={taskD.id} Data={taskD} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,37 @@
-import React from "react";
-
 function NavBar() {
   return (
-    <nav className="w-full h-13 flex justify-between items-center px-8 bg-gray-950 text-white">
-      <div className="nav-heading text-4xl font-medium underline-offset-4 decoration-1 underline decoration-bg-second">Just Do it.</div>
-      <div className="nav-links flex gap-4 **:font-medium">
-        <ul className="flex gap-1 items-center text-[18px] *:px-3 *:py-2 *:hover:bg-gray-900">
-            <li>
-                <a href="#">Home</a>
-            </li>
-            <li>
-                <a href="#">Product</a>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
+    <nav className="
+      w-full h-13 flex justify-between items-center px-8
+      bg-bg-primary-2-dark text-white
+    ">
+      <div className="
+        nav-heading text-4xl font-medium
+        underline underline-offset-4
+        decoration-bg-secondary
+      ">
+        Just Do it.
+      </div>
+
+      <div className="nav-links flex gap-4 items-center">
+        <ul className="
+          flex items-center text-[18px]
+          *:px-3 *:py-2
+          *:rounded-md
+          *:hover:bg-accent-ink
+          *:transition-colors
+        ">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Product</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
-        <button className="prime-btn rounded-md">Log Out</button>
+
+        <button className="prime-btn px-4 py-2 rounded-md">
+          Log Out
+        </button>
       </div>
     </nav>
+
   );
 }
 
